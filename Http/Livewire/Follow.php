1efd7ae6411f15ba19d6370000b599569981afe4
Cristian Marin <marin101998@gmail.com>
2024-05-21 12:35:21 -0500

    +update | v10.x 21-05-24 Optimize blade and livewire components

--- Http/Livewire/Follow.php
+++ Http/Livewire/Follow.php
@@ -7,117 +7,112 @@ use Livewire\Component;
 
 class Follow extends Component
 {
-    /**
-     * Attributes
-     */
-    public $isLogged;
+  /**
+   * Attributes
+   */
+  public $isLogged;
+  public $isFollowing;
+  public $followableType;
+  public $followableId;
+  public $followerId;
+  public $needRedirectToLogin;
 
-    public $isFollowing;
+  /*
+  * Runs once, immediately after the component is instantiated,
+  * but before render() is called
+  */
+  public function mount($followableId, $followableType)
+  {
+    $this->isFollowing = false;
+    $this->needRedirectToLogin = false;
+    $this->followableId = $followableId;
+    $this->followableType = $followableType;
+    $this->identityFollow();
+    $this->isFollowing();
+  }
 
-    public $followableType;
+  //user loguin
+  public function identityFollow()
+  {
+    $this->needRedirectToLogin = false;
+    $user = Auth::user();
 
-    public $followableId;
+    //if isset an user authenticated
+    $this->followerId = $user->id ?? null;
 
-    public $followerId;
+    if (empty($this->followerId)) {
+      $this->needRedirectToLogin = true;
+    }
+  }
 
-    public $needRedirectToLogin;
+  public function isFollowing()
+  {
+    $repository = app("Modules\Ifollow\Repositories\FollowerRepository");
+    $params = [
+      'filter' => [
+        'field' => 'followable_id',
+        'followableType' => $this->followableType,
+        'followerId' => $this->followerId,
+      ],
+    ];
+    $follower = $repository->getItem($this->followableId, json_decode(json_encode($params)));
+    //  $this->identityFollow();
 
-    /*
-    * Runs once, immediately after the component is instantiated,
-    * but before render() is called
-    */
-    public function mount($followableId, $followableType)
-    {
-        $this->isFollowing = false;
-        $this->needRedirectToLogin = false;
-        $this->followableId = $followableId;
-        $this->followableType = $followableType;
-        $this->identityFollow();
-        $this->isFollowing();
+    if (isset($follower->id)) {
+      $this->isFollowing = true;
+    } else {
+      $this->isFollowing = false;
+    }
+  }
+
+  //create following
+  public function setNewFollower()
+  {
+    $this->identityFollow();
+    if ($this->needRedirectToLogin) {
+      return redirect()->to('/auth/login');
     }
 
-    //user loguin
-    public function identityFollow()
-    {
-        $this->needRedirectToLogin = false;
-        $user = Auth::user();
+    $repository = app("Modules\Ifollow\Repositories\FollowerRepository");
+    $data = [
+      'follower_id' => $this->followerId,
+      'followable_id' => $this->followableId,
+      'followable_type' => $this->followableType,
+    ];
+    $repository->create($data);
+    $this->isFollowing = true;
+    $this->emit('updateFollowers');
+  }
 
-        //if isset an user authenticated
-        $this->followerId = $user->id ?? null;
-
-        if (empty($this->followerId)) {
-            $this->needRedirectToLogin = true;
-        }
+  //Delete following
+  public function deleteFollower()
+  {
+    $this->identityFollow();
+    if ($this->needRedirectToLogin) {
+      return redirect()->to('/auth/login');
     }
 
-    public function isFollowing()
-    {
-        $repository = app("Modules\Ifollow\Repositories\FollowerRepository");
-        $params = [
-            'filter' => [
-                'field' => 'followable_id',
-                'followableType' => $this->followableType,
-                'followerId' => $this->followerId,
-            ],
-        ];
-        $follower = $repository->getItem($this->followableId, json_decode(json_encode($params)));
-        //  $this->identityFollow();
+    $repository = app("Modules\Ifollow\Repositories\FollowerRepository");
+    $params = [
+      'filter' => [
+        'field' => 'followable_id',
+        'followableType' => $this->followableType,
+        'followerId' => $this->followerId,
+      ],
+    ];
+    $repository->deleteBy($this->followableId, json_decode(json_encode($params)));
+    $this->isFollowing = false;
+    $this->emit('updateFollowers');
+  }
 
-        if (isset($follower->id)) {
-            $this->isFollowing = true;
-        } else {
-            $this->isFollowing = false;
-        }
-    }
+  /*
+  * Render
+  * return view
+  */
+  public function render()
+  {
+    $view = 'ifollow::frontend.livewire.followers';
 
-    //create following
-    public function setNewFollower()
-    {
-        $this->identityFollow();
-        if ($this->needRedirectToLogin) {
-            return redirect()->to('/auth/login');
-        }
-
-        $repository = app("Modules\Ifollow\Repositories\FollowerRepository");
-        $data = [
-            'follower_id' => $this->followerId,
-            'followable_id' => $this->followableId,
-            'followable_type' => $this->followableType,
-        ];
-        $repository->create($data);
-        $this->isFollowing = true;
-        $this->emit('updateFollowers');
-    }
-
-    //Delete following
-    public function deleteFollower()
-    {
-        $this->identityFollow();
-        if ($this->needRedirectToLogin) {
-            return redirect()->to('/auth/login');
-        }
-
-        $repository = app("Modules\Ifollow\Repositories\FollowerRepository");
-        $params = [
-            'filter' => [
-                'field' => 'followable_id',
-                'followableType' => $this->followableType,
-                'followerId' => $this->followerId,
-            ],
-        ];
-        $repository->deleteBy($this->followableId, json_decode(json_encode($params)));
-        $this->isFollowing = false;
-        $this->emit('updateFollowers');
-    }
-
-    /*
-    * Render
-    * return view
-    */
-    public function render()
-    {
-        $view = 'ifollow::frontend.livewire.followers';
-
-        return view($view);
-    }
+    return view($view);
+  }
 }
