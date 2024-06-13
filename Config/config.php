9d92f17f37b277e260091fe08d068d93572fb01c
William <williamverde10@gmail.com>
2024-06-13 11:54:34 -0400

    Added: notification source in config

--- Config/config.php
+++ Config/config.php
@@ -2,4 +2,13 @@
 
 return [
 
+/*
+|--------------------------------------------------------------------------
+| Notification Source Configuration
+|--------------------------------------------------------------------------
+*/
+  'notificationSource' => [
+    'ifollow' => ['label' => 'Seguidores', 'color' => 'green', 'icon' => 'fa-light fa-message']
+  ],
+
 ];
